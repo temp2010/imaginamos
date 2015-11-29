17b093fb16be19be35c0687eae4be41526e26bfa
José Daniel Peña <temp2010@msn.com>
2015-11-29 10:31:31 -0500

    Correccion: modulos de contactenos compartido

---
+++ frontend/models/Contactenos.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Contactenos".
+ *
+ * @property integer $id
+ * @property string $nombre
+ * @property string $correo
+ * @property string $mensaje
+ */
+class Contactenos extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Contactenos';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['nombre', 'correo', 'mensaje'], 'required'],
+            [['mensaje'], 'string'],
+            [['nombre', 'correo'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'correo' => 'Correo',
+            'mensaje' => 'Mensaje',
+        ];
+    }
+}
